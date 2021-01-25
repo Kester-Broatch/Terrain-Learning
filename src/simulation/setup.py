26f1c00ed7d5345a589660c62f4eaa76acc1c1d4
Kester-Broatch <kesterbroatch@gmail.com>
2021-01-25 23:51:07 +0100

    Added open3d library

--- src/simulation/setup.py
+++ src/simulation/setup.py
@@ -5,6 +5,9 @@
 
 import os
 import re
+import numpy as np
+from open3d import camera, geometry, io
+import matplotlib.pyplot as plt
 
 project_path = os.path.dirname(os.path.realpath(__file__)).replace('/src/simulation','')
 data_paths = [project_path+"/data/test", project_path+"/data/train"] # Relative to this file
@@ -20,11 +23,22 @@ def clean_names(path):
                 os.rename(dirpath+"/"+filename, dirpath+"/"+new_filename)
                 # print(filename + "  ---->  " + new_filename)
 
+def depth2pcloud(path):
+    print("Calculating point clouds from ... "+path)    
+    phc = camera.PinholeCameraIntrinsic()
+    for (dirpath, dirnames, filenames) in os.walk(path):  
+        for filename in filenames:
+            depth = io.read_image(dirpath+"/"+filename) 
+            np_depth = np.array(depth)
+            phc.set_intrinsics(np_depth.shape[1],np_depth.shape[0],3.8,3.8,np_depth.shape[1]/2,np_depth.shape[0]/2)
+            pcloud = geometry.PointCloud()
+            pcloud.create_from_depth_image(np_depth, phc)
+
+          
 def main():
     for data_path in data_paths:
         clean_names(data_path)
-
-
+        depth2pcloud(data_path+"/depth_gray")
 
 
 if __name__ == "__main__":
